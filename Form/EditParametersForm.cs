1aa407b9acbdbbf6951d4a837713b8026b5a5a80
ivan-gabavan <53791165+ivan-gabavan@users.noreply.github.com>
2022-08-24 14:14:47 +0300

    Add files via upload

---
+++ Form/EditParametersForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using Jacobi.Vst.Interop.Host;
+using Jacobi.Vst.Core;
+
+namespace B.GUI
+{
+	public partial class EditParametersForm : Form
+	{
+		public EditParametersForm()
+		{
+			InitializeComponent();
+		}
+
+		private void EditParametersForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			e.Cancel = true;
+			Visible = false;
+		}
+
+		public void AddParameters(VstPluginContext pluginContext)
+		{
+			lbParameters.Items.Clear();
+
+			for (int i = 0; i < pluginContext.PluginInfo.ParameterCount; i++)
+			{
+				var param = new VSTParameter();
+				param.Properties = pluginContext.PluginCommandStub.GetParameterProperties(i);
+				param.Name = pluginContext.PluginCommandStub.GetParameterName(i);
+				param.Label = pluginContext.PluginCommandStub.GetParameterLabel(i);
+				param.Display = pluginContext.PluginCommandStub.GetParameterDisplay(i);
+
+				lbParameters.Items.Add(param);
+			}
+		}
+	}
+	
+	public struct VSTParameter
+	{
+		public int Index;
+
+		public string Name;
+		public string Label;
+		public string Display;
+
+		public VstParameterProperties Properties;
+
+		public override string ToString()
+		{
+			return Name+" = "+Display+Label;
+		}
+	}
+}
---
+++ Form/MixerForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace B.GUI
+{
+    public partial class MixerForm : Form
+    {
+        WaveChannel32 waveChannel = null;
+
+        public MixerForm(WaveChannel32 channel)
+        {
+            InitializeComponent();
+
+            waveChannel = channel;
+            tbPan.Value = 50+(int)(channel.Pan*50.0f);
+            tbVolume.Value = (int)(channel.Volume*100.0f);
+        }
+
+        private void tbPan_ValueChanged(object sender, EventArgs e)
+        {
+            waveChannel.Pan = ((float)tbPan.Value / 50.0f) - 1.0f;
+        }
+
+        private void MixerForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel=true;
+            Visible = false;
+        }
+
+        private void tbVolume_ValueChanged(object sender, EventArgs e)
+        {
+            waveChannel.Volume = ((float)tbVolume.Value) / 100.0f;
+        }
+    }
+}
